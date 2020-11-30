c317fc831d485be2f7b29bcb274246033aa04455
Daniel Avelino <daniel_avelino@outlook.com>
2020-11-30 15:40:23 -0300

    test

---
+++ src/main/java/com/ticketlogapi/TicketlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.ticketlogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketlogApiApplication.class, args);
+	}
+
+}
